5f94267163a9fda63ba270188b016a9a97de93f0
neetmehta <neetmehta19@gmail.com>
2022-05-15 09:32:51 -0400

    voc

--- data.py
+++ data.py
@@ -117,7 +117,7 @@ class VOCDataset(torch.utils.data.Dataset):
 
         if self.transform:
             # image = self.transform(image)
-            image, boxes = self.transform(image, boxes)
+            image = self.transform(image)
 
         # Convert To Cells
         label_matrix = torch.zeros((self.S, self.S, self.C + 5 * self.B))
